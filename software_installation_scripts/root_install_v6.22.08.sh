8831b3481fedd96e660b3432ff1018d3ef045470
root <root@galactica.physics.uu.se>
2021-04-13 14:28:16 +0200

    Build the script in /etc/profile.d only if make install succeeds.

--- software_installation_scripts/root_install_v6.22.08.sh
+++ software_installation_scripts/root_install_v6.22.08.sh
@@ -3,12 +3,13 @@
 V=6.22.08
 PREFIX=/usr/local/root_v${V}
 ARCHIVE=root_v${V}.source.tar.gz
+F=${PREFIX}/bin/thisroot.sh
 
 wget -q https://root.cern/download/${ARCHIVE}
 tar xzf ${ARCHIVE}
 mkdir build
 cd build
-cmake ../root-${V} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -Dbuiltin_openssl=ON
-make
-make install
-echo ". ${PREFIX}/bin/thisroot.sh" >> /etc/profile.d/root_v${V}.sh
+cmake ../root-${V} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release \
+&& make \
+&& make install \
+&& echo "if [ -f ${F} ]; then . ${F}; fi" > /etc/profile.d/root_v${V}.sh
