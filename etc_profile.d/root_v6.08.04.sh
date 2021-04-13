0c65032b35d2824d41a002edb7e07756e4259df0
root <root@galactica.physics.uu.se>
2021-04-13 18:16:31 +0200

    Root version 6.22/08 installed.

--- etc_profile.d/root_v6.08.04.sh
+++ etc_profile.d/root_v6.08.04.sh
@@ -1,1 +1,1 @@
-. /usr/local/root_v6.08.04/bin/thisroot.sh
+#. /usr/local/root_v6.08.04/bin/thisroot.sh
---
+++ etc_profile.d/root_v6.22.08.sh
@@ -0,0 +1,1 @@
+if [ -f /usr/local/root_v6.22.08/bin/thisroot.sh ]; then . /usr/local/root_v6.22.08/bin/thisroot.sh; fi
