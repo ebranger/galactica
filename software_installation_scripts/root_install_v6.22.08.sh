4c370e83e95553c5f154d2fb7468c20c2a2cf893
root <root@galactica.physics.uu.se>
2021-04-13 13:24:54 +0200

    roofit is ON by default. Setting builtin_openssl to ON.

--- software_installation_scripts/root_install_v6.22.08.sh
+++ software_installation_scripts/root_install_v6.22.08.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#yum install mesa-libGLU mesa-libGLU-devel
-
 V=6.22.08
 PREFIX=/usr/local/root_v${V}
 ARCHIVE=root_v${V}.source.tar.gz
@@ -10,7 +8,7 @@ wget https://root.cern/download/${ARCHIVE}
 tar xzf ${ARCHIVE}
 mkdir build
 cd build
-cmake ../root-${V}/ -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -Droofit=ON
-make -j
+cmake ../root-${V} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -Dbuiltin_openssl=ON
+make
 make install
 echo ". ${PREFIX}/bin/thisroot.sh" >> /etc/profile.d/root_v${V}.sh
