5d8d8241c5715637fc885ebfc3de44946a7572b9
root <root@galactica.physics.uu.se>
2021-07-01 21:44:35 +0200

    Fluka's licence is of single user type?

--- etc_profile.d/fluka.sh
+++
@@ -1,4 +0,0 @@
-#fluka-installation on Andrea's account
-alias flukahp="/home/mattera/programs/Fluka/flukahp"
-alias flair="/home/mattera/programs/bin/flair"
-export FLUPRO=/home/mattera/programs/Fluka
